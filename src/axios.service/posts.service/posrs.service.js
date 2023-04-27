316f5dcbfa7dc328d6c7a1b6e1478495bd153fed
boyko <boykoandriy93@gmail.com>
2023-04-27 10:06:05 +0300

    master

--- src/axios.service/posts.service/posrs.service.js
+++
@@ -1,5 +0,0 @@
-import axios from "axios";
-
-export const postsService = {
-    getAll: (userId)=> axios(`https://jsonplaceholder.typicode.com/users/${userId}/posts`)
-}
--- src/axios.service/user.service/user.service.js
+++
@@ -1,5 +0,0 @@
-import axios from "axios";
-
-export const userService = {
-    gertAll: () => axios('https://jsonplaceholder.typicode.com/users')
-}
