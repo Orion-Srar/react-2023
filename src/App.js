534fe35115d51fdb05315bd81fbb21b18af75ec7
boyko <boykoandriy93@gmail.com>
2023-04-27 18:56:17 +0300

    master

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
-import React, {useState} from 'react';
-import Users from "./components/users/Users";
-import Posts from "./components/posts/Posts";
-
 const App = () => {
 
-    let [user, setUser] = useState({});
 
-    function lift(obj) {
-        setUser(obj)
-    }
 
     return (
         <div>
-            <Users lift={lift}/>
-            <Posts user={user}/>
+
         </div>
     );
 };
